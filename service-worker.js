acb5dc41351c34da6d733781a25cbbb029425e3a
anhtho25 <Ntho0124@gmail.com>
2025-06-14 16:03:32 +0700

    Update: Thêm

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,55 @@
-const CACHE_NAME = 'am-coffee-v1';
+const CACHE_NAME = 'am-coffee-v2';
 const urlsToCache = [
   '/',
   '/index.html',
   '/css/style.css',
   '/css/bootstrap.css',
+  '/css/mobile.css',
   '/js/script.min.js',
-  '/images/logo.png'
-  // Thêm các file tĩnh khác nếu cần
+  '/images/logo.png',
+  '/manifest.json',
+  // Thêm các breakpoint images nếu có
+  '/images/logo-small.png',
+  '/images/logo-medium.png',
+  '/images/logo-large.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
-}); 
+});
+
+// Clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
